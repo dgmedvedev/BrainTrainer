04ae335afd8bc70b461c0d2884fa65fd79b3e225
dgmedvedev <dg-medvedev@mail.ru>
2023-03-15 13:15:55 +0300

    Created project

---
+++ app/src/main/java/com/demo/braintrainer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.demo.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit().putInt("test", 5).apply();
+        int test = preferences.getInt("test", 0);
+        Toast.makeText(this, Integer.toString(test), Toast.LENGTH_SHORT).show();
+    }
+}
