1111ce21d9c36f21ba116db579b3cc3a53df8a45
dgmedvedev <dg-medvedev@mail.ru>
2023-03-15 18:58:56 +0300

    add CountDownTimer

--- app/src/main/java/com/demo/braintrainer/MainActivity.java
+++ app/src/main/java/com/demo/braintrainer/MainActivity.java
@@ -2,21 +2,39 @@ package com.demo.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTimer = findViewById(R.id.textViewTimer);
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        preferences.edit().putInt("test", 5).apply();
-        int test = preferences.getInt("test", 0);
-        Toast.makeText(this, Integer.toString(test), Toast.LENGTH_SHORT).show();
+        // создание таймера с помощью абстрактного класса
+        // 2 параметра: 1-сколько милисек будет отсчитывать таймер, 2-как часто будет тикать таймер
+        CountDownTimer timer = new CountDownTimer(6000, 1000) {
+            // onTick() принимает кол-во милисек оставшихся до завершения работы таймера
+            @Override
+            public void onTick(long millisUtilFinished) {
+                int sec = (int) (millisUtilFinished / 1000);
+                sec++;
+                String seconds = Integer.toString(sec);
+                textViewTimer.setText(seconds);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "Таймер завершен", Toast.LENGTH_SHORT).show();
+                textViewTimer.setText("0");
+            }
+        };
+        timer.start();
     }
 }
