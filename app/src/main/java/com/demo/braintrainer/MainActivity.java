39fdd10d3ba12cea39b00deaa66acb9521f5f18d
dgmedvedev <dg-medvedev@mail.ru>
2023-03-16 13:17:35 +0300

    add createTask()

--- app/src/main/java/com/demo/braintrainer/MainActivity.java
+++ app/src/main/java/com/demo/braintrainer/MainActivity.java
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewScore;
     TextView textViewTimer;
+    TextView textViewTask;
+    TextView textViewOption1;
+    TextView textViewOption2;
+    TextView textViewOption3;
+    TextView textViewOption4;
+
+    int countQuestion;
+    int countAnswer;
+    int a;
+    int b;
+    boolean isPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewScore = findViewById(R.id.textViewScore);
         textViewTimer = findViewById(R.id.textViewTimer);
+        textViewTask = findViewById(R.id.textViewTask);
+        textViewOption1 = findViewById(R.id.textViewOption1);
+        textViewOption2 = findViewById(R.id.textViewOption2);
+        textViewOption3 = findViewById(R.id.textViewOption3);
+        textViewOption4 = findViewById(R.id.textViewOption4);
+
 
         // создание таймера с помощью абстрактного класса
         // 2 параметра: 1-сколько милисек будет отсчитывать таймер, 2-как часто будет тикать таймер
@@ -36,5 +56,25 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         timer.start();
+
+        createTask();
+    }
+
+    private void createTask() {
+        if (isPlus) {
+            sum(a, b);
+        } else {
+            diff(a, b);
+        }
+    }
+
+    private void sum(int a, int b) {
+        int c = a + b;
+        Log.i("SUM_", Integer.toString(c));
+    }
+
+    private void diff(int a, int b) {
+        int c = a - b;
+        Log.i("DIFF_", Integer.toString(c));
     }
 }
